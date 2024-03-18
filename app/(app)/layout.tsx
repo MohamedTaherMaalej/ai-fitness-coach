39169148be6fdb2b0c7ea4e608fce0dcdf5abbcd
Mohamed Taher MAALEJ <mohamedtahermaalej@gmail.com>
2024-03-18 23:58:25 +0100

    feat: Add axios package

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,10 +1,41 @@
+'use client'
+
 import Navbar from "@/components/Navbar";
+import { UserThread } from "@prisma/client";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function AppLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
+    const [userThread, setUserThread] = useState<UserThread | null>(null);
+
+    useEffect(() => {
+
+        async function getUserThread() {
+            try {
+                const response = await axios.get<{success: boolean; message?: string: userThread: UserThread;}>("/api/user-thread");
+                if (!response.data.success || !response.data.userThread) {
+                    console.error(response.data.message ?? "Unknown error.");
+                    setUserThread(null);
+                    return;
+                }
+                
+                setUserThread(response.data.userThread);
+
+            } catch (error) {
+                console.error(error);
+                setUserThread(null);
+            }
+
+        }
+
+        getUserThread();
+        
+    } , []);
+
     return (
         <div className="flex flex-col w-full h-full">
             <Navbar />
--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+'use client'
+
+import { ThreadMessage } from 'openai/resources/beta/threads/index.mjs'
+import React, { useState } from 'react'
 
 function ChatPage() {
+  const [fetching, setFetching] = useState(true)
+  const [messages, setMessages] = useState<ThreadMessage>([])
+
+  const fetchMessages = async () => {
+    const response = await fetch('/api/messages')
+    const data = await response.json()
+    setMessages(data)
+    setFetching(false)
+  }
+
   return (
-    <div>ChatPage</div>
+    <div className="w-screen h-screen flex flex-col bg-black text-white">
+      <div className="flex-grow overflow-y-hidden p-8 space-y-8">
+        {fetching && <div className='text-center font-blod'>Fetching...</div>}
+      </div>
+      
+    </div>
+
   )
 }
 
